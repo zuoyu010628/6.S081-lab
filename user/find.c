bab221e7f6317b1e31cc778603a9c7bcece1248c
joe <zuoyusile@gmail.com>
2022-04-29 21:10:29 +0800

    finish find but look other`s code(sad)

--- user/find.c
+++ user/find.c
@@ -3,57 +3,72 @@
 #include"kernel/stat.h"
 #include"kernel/fs.h"
 
-void find(char *path, char *name) {
-    char buf[512];
+void find(char *path, char *name) {//from ls to find
+    char buf[512], *p;
     int fd;
     struct stat st;//stat is something have some information about file
     struct dirent de;//about directory? tell you file id and name in this directory 
 
     //error hint
-    if ((fd = open(path, 0)) < 0) {//open path
+    if ((fd = open(path, 0)) <  0) {//open path
         fprintf(2, "find: can not open %s\n", path);
         //exit(1);
         return;
     }
 
-    if (strlen(path) + 1 + DIRSIZ + 1 > sizeof (buf)) {//i don`t know why
-        //printf("%d, %d, %d\n", strlen(path), DIRSIZ, sizeof(buf));
-        printf("find: path too long \n");
+    if(fstat(fd, &st) < 0) {//get stat about now flie
+        fprintf(2, "find: can not stat %s\n", path);
+        close(fd);
+        //exit(1);
         return;
     }
 
-    while (read(fd, &de, sizeof(de)) == sizeof(de)) {//search all files in this directory
+    switch(st.type) {
+        case T_FILE:
+        fprintf(2, "usage: find <directory> <filename>\n");
+        exit(1);
+        break;
+        case T_DIR:
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof (buf)) {//i don`t know why
+        //printf("%d, %d, %d\n", strlen(path), DIRSIZ, sizeof(buf));
+        printf("find: path too long \n");
+        return;
+        }
+        strcpy(buf, path);
+        p = buf + strlen(buf);
+        *p++ = '/';
+        while (read(fd, &de, sizeof(de)) == sizeof(de)) {//search all files in this directory
         //printf("ok\n");
-        if (de.inum == 0) {// copy from ls.c & why? maybe 0 don`t belong to any file
-            continue;
-        }
+            if (de.inum == 0 || strcmp(".", de.name) == 0 || strcmp("..", de.name) == 0) {// copy from ls.c & why? maybe 0 don`t belong to any file
+                continue;
+            }
 
-        if(stat(de.name, &st) < 0) {//get stat about now flie
-            fprintf(2, "find: can not stat %s\n", path);
-            close(fd);
-            //exit(1);
-            return;
-        }
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
 
-        if (st.type == T_DIR) {//is a dir , find in the dir
-            if (!strcmp("." , de.name) && !strcmp("..", de.name)) {
-                printf("this is a dir\n");
-                find(de.name, name);
+            if (stat(buf, &st) < 0) {
+                fprintf(2, "find: can not stat %s", buf);
+                continue;
+            }
+
+            if (st.type == T_FILE) {
+                if(strcmp(de.name, name) == 0) {
+                    printf("%s\n", buf);
+                }
+            }
+            else if (st.type == T_DIR) {
+                find(buf, name);
             }
         }
-
-        if (!strcmp(name, de.name)) {
-            printf("%s/%s\n", path, de.name);
-        }
+        close(fd);
     }
-
-    close(fd);
+    
 }
 
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
-        fprintf(2, "usage: find ... \n");
+        fprintf(2, "usage: find <directory> <filename> \n");
         exit(1);
     }
 
