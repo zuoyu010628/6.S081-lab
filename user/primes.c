7bfc7c30eef69426de29a6484cbda9f8caba3430
joe <zuoyusile@gmail.com>
2022-05-04 20:37:20 +0800

    finsh primes & lab1

--- user/primes.c
+++ user/primes.c
@@ -6,24 +6,38 @@ void go(int p[2])
     close(p[1]);
 
     int l;
-    read(p[0], &l, 4);
+    if ((read(p[0], &l, 4)) != 4) {
+        fprintf(2, "primes: failed to read from parent\n");
+        exit(1);
+    }
     printf("prime %d\n", l);
     
     int l2r[2];
     int n;
-
-    while (read(p[0], &n, 4))
+    int flag = 0;
+    while (read(p[0], &n, 4) == 4)
     {
         if (n % l != 0) {
-            pipe(l2r);
-            if (fork() == 0) {
-                go(l2r);
+            if (flag == 0) {
+                pipe(l2r);
+                if (fork() == 0) {
+                    close(p[0]);
+                    go(l2r);
+                }
+                else {
+                    close(l2r[0]);
+                    flag = 1;
+                }
             }
-            else {
-                write(l2r[1], &n, 4);
-            }
+            write(l2r[1], &n, 4);
         }
     }
+
+    close(p[0]);
+    close(l2r[1]);
+    //wait for children return
+    while (wait(0) != -1) ;
+    exit(0);
 }
 
 int main()
@@ -34,7 +48,6 @@ int main()
     if (fork() == 0)
     { // child
         go(p);
-        close(p[0]);
     }
     else
     { // parent
@@ -44,6 +57,7 @@ int main()
             write(p[1], &i, 4); // int 4bytes
         }
         close(p[1]);
+        wait(0);
     }
     exit(0);
 }
